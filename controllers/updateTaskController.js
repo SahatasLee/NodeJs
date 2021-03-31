f13f39f930317830cb42de5c1aafac0d9fae3f58
SahatasLee <Sahataslee@outlook.co.th>
2021-03-31 09:23:12 +0700

    modify _id

--- controllers/updateTaskController.js
+++ controllers/updateTaskController.js
@@ -1,6 +1,6 @@
 const { updateTaskService } = require('../services');
 
 module.exports = async function updateTaskController(req,res){
-    await updateTaskService(req.query.id, req.body);
+    await updateTaskService(req.query._id, req.body);
     return res.sendStatus(200);
 }
--- services/updateTaskService.js
+++ services/updateTaskService.js
@@ -1,5 +1,6 @@
 const { TaskModel } = require("../models");
 
 module.exports = async function updateTaskController(id, data){
+    console.log(id, data)
     const task = await TaskModel.findOneAndUpdate({ _id: id}, data);
 }
